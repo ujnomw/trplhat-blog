74015d143a0ce6caf30a229028d06f366f929a9f
ujnomw <m.golovatskih@gmail.com>
2017-09-16 20:50:31 +0300

    Security fixes, drafts added

--- trplhat/settings.py
+++ trplhat/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog' 	
+    'blog'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +101,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
