ee7d3cb304d7a0bc1ba3232988ed7d3fd7b3b6b5
ujnomw <m.golovatskih@gmail.com>
2017-09-17 22:31:31 +0300

    Post list ordered by publish date: new is first

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
@@ -85,4 +85,4 @@ def comment_approve(request, pk):
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail', pk=comment.post.pk)    
+    return redirect('post_detail', pk=comment.post.pk)
